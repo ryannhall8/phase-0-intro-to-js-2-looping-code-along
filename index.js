e802127c5caa7ab82b5c81bf608f4834558df494
Ryann <ryann.hall2004@gmail.com>
2024-12-09 21:11:19 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  }
+
+  const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+console.log(writeCards(names));
+
+function countDown(start) {
+    let i = start;
+    while(i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+countDown(10);
